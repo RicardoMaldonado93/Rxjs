e39874963558354971f40ccd50e84c731d6ddb75
Your Name <you@example.com>
2018-11-26 21:31:57 -0300

    Observable, observer, operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'g';
+
+  observable$ = new Observable( subscriber => {
+  
+    let array = [1,2,3,4,5,6,7,8,9,10];
+    
+    
+
+    array.forEach((element)=>{
+      subscriber.next(element)
+    });
+
+    subscriber.complete();
+
+
+  });
+
+  constructor(){
+
+      var obs = {
+
+      next(msg){
+          console.log(msg);
+      },
+      complete(){}
+        
+    };
+
+    this.observable$.pipe(
+      //map( nro=> <number>nro * 2 ),
+      //ilter( nro=> <number>nro >= 4),
+      filter( nro=> <number>nro%2 ==0 )
+    )
+    .subscribe(obs);
+  }
 }
